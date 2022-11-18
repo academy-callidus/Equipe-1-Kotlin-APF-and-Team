7163ebb43839d403e7a500ad4bc9961572ef1a67
Paulo Eduardo Ribeiro Cativo <71790704+Cativante@users.noreply.github.com>
2022-11-17 23:07:01 -0400

    Update SerialCmd.kt

--- automacao/app/src/main/java/br/com/tectoy/tectoysunmi/activity/SerialCmd.kt
+++ automacao/app/src/main/java/br/com/tectoy/tectoysunmi/activity/SerialCmd.kt
@@ -1,10 +1,17 @@
 package br.com.tectoy.tectoysunmi.activity
-
+/**
+ * created by mayflower on 2020.6.23
+ * convertido para Kotlin by PauloCativo on 17/11/2022
+ * get me at jiangli@sunmi.com
+ * 这里封装了基本的语法和常用指令，别的详见指令集文档
+ */
 class SerialCmd {
-    val PREFIX_HEX:String="7E0130303030"
-    val TOSRAGE_EVER_HEX:String="40"
-    val STORAGE_TEMP_HEX:String="23"
-    val SUFFIX_HEX:String="3B03"
+    //命令格式：Prefix Storage Tag SubTag {Data} [, SubTag {Data}] [; Tag SubTag {Data}] […] ; Suffix
+    val PREFIX_HEX:String="7E0130303030" //~<SOH>0000
+    val TOSRAGE_EVER_HEX:String="40" //@ 是设置永久有效
+    val STORAGE_TEMP_HEX:String="23" //# 则是临时设置，断电后失效
+    //tag、subtag、data，这三个合起来就是具体的命令；多个命令可以合起来发，也可以挨个发（需间隔50ms或收到返回值后发）
+    val SUFFIX_HEX:String="3B03"  //;<ETX>
 
     val SUBTAG_QUERY_CURRENT_HEX:String="2A"
     val SUBTAG_QUERY_FACTORY_HEX:String="25"
@@ -32,8 +39,21 @@ class SerialCmd {
      */
 
     companion object  {
+        /**
+         * 设置识读偏好,出厂默认是扫屏模式(曝光等级低,LV5),可以改成纸质模式(曝光等级高,LV0)
+         */
         fun setExposure(level:Int): String { return "@EXPLVL" + level.toString()}
 
+        /**
+         * 以下是其他常用指令，本扫码器独有
+         * 设置扫码模式
+         * @param mode 0 电平触发模式，手动读码（发1B31才读一次码）
+         *             1 自动触发模式，自动读码
+         * @param wait 0 亮灯后等待时间默认值，手动读码模式默认60000ms，自动读码模式默认1000ms
+         *             $ 等待时间 ms
+         * @param delay 0 同码间隔时间默认值(异码间隔默认200，一般不需要改)，自动读码默认1000ms，只对自动读码有效
+         *              $ 扫码间隔 ms
+         */
         fun setScanMode(mode:Int,wait:Int,delay:Int):String? {
             when (mode) {
                 0 -> {
@@ -59,7 +79,10 @@ class SerialCmd {
                 }
             }
         }
-
+        /**
+         * 查询当前配置
+         * @return
+         */
         fun queryScanMode(): String {
             return "#SCNMOD*;ORTSET*;RRDDUR*"
         }
